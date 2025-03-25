694955ebe6f70fe82cf5d60902635c92008be31d
DoDuy2004 <duy2004.gi@gmail.com>
2025-03-25 17:34:00 +0700

    add nav

--- src/configs/navigationConfig.ts
+++ src/configs/navigationConfig.ts
@@ -1,25 +1,33 @@
-import i18n from '@i18n';
-import { FuseNavItemType } from '@fuse/core/FuseNavigation/types/FuseNavItemType';
-import ar from './navigation-i18n/ar';
-import en from './navigation-i18n/en';
-import tr from './navigation-i18n/tr';
+import i18n from "@i18n";
+import { FuseNavItemType } from "@fuse/core/FuseNavigation/types/FuseNavItemType";
+import ar from "./navigation-i18n/ar";
+import en from "./navigation-i18n/en";
+import tr from "./navigation-i18n/tr";
 
-i18n.addResourceBundle('en', 'navigation', en);
-i18n.addResourceBundle('tr', 'navigation', tr);
-i18n.addResourceBundle('ar', 'navigation', ar);
+i18n.addResourceBundle("en", "navigation", en);
+i18n.addResourceBundle("tr", "navigation", tr);
+i18n.addResourceBundle("ar", "navigation", ar);
 
 /**
  * The navigationConfig object is an array of navigation items for the Fuse application.
  */
 const navigationConfig: FuseNavItemType[] = [
-	{
-		id: 'example-component',
-		title: 'Example',
-		translate: 'EXAMPLE',
-		type: 'item',
-		icon: 'heroicons-outline:star',
-		url: 'example'
-	}
+  {
+    id: "example-component",
+    title: "Example",
+    translate: "EXAMPLE",
+    type: "item",
+    icon: "heroicons-outline:star",
+    url: "example",
+  },
+  {
+    id: "customers",
+    title: "Customers",
+    translate: "CUSTOMERS",
+    type: "item",
+    icon: "heroicons-outline:star",
+    url: "customers",
+  },
 ];
 
 export default navigationConfig;
